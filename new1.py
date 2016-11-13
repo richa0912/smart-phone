724ac430f70586ab59032c7083572aa9ad3b000e
richa0912 <richa09manral@gmail.com>
2016-11-13 10:11:14 +0530

    sessions

--- new1.py
+++ new1.py
@@ -15,111 +15,130 @@ mysql.init_app(app)
 mysql.connect().autocommit(True)
 token="274697834:AAHhDcqLAQ0fosM45R6haddl8U64smE62b4"
 bot=telegram.Bot(token)
+chat_id="null"
 @app.route('/274697834:AAHhDcqLAQ0fosM45R6haddl8U64smE62b4/webhook',methods=['get','post'])
 def token():
+	global chat_id
 	content=request.json
-	if 'chat_id' in session:
-		if session['i']%5==0:
-			reply_markup = ReplyKeyboardMarkup([[telegram.KeyboardButton('Do you want to share Location', request_location=True)]],one_time_keyboard=False,resize_keyboard=True)
-			bot.sendMessage(session['chat_id'], 'Sorry for interrupting!'+name, reply_markup=reply_markup)
-			
-			
-		if "callback_query" in content:
-			t=time.time()
-			session['qtime']=session['qtime']-t
-			gans=content['callback_query']['data']
-			tcheck=content['callback_query']['message']['date']
-			internet_check=t-tcheck
-			if (t-int(internet_check))>10000 and session['i']%4==0:
-				bot.sendMessage(session['chat_id'], "It seems your internet not working properly. Don't worry your current status is saved:)" )	
-			checkans(str(gans)[0])
-			#print data
-			#i=i+1
-			takeques()
+	#print session
+	if "callback_query" in content:
+		t=time.time()
+		chat_id=str(content['callback_query']['message']['chat']['id'])
+	elif 'location' in content['message']:
+		chat_id=str(content['message']['chat']['id'])
 	
-		elif 'text' in content['message']:
-			fname=content['message']['chat']['first_name']
-			lname=content['message']['chat']['last_name']
-			name=fname+" "+lname
-			msg = content['message']['text']
-			start(name,msg)
-			
-		elif 'location' in content['message']:
-			longi = content['message']['location']['longitude']
-			lati = content['message']['location']['latitude']
-			fname=content['message']['chat']['first_name']
-			lname=content['message']['chat']['last_name']
-			name=fname+" "+lname
-			geolocator = GoogleV3()
-			#location = geolocator.reverse("52.509669, 13.376294")
-			location = geolocator.reverse((lati,longi))
-			adduser(session['chat_id'],name,str(location[0]))
-			bot.sendMessage(session['chat_id'], text="The quiz is about to start.")
-			giveques(session['i'],session['chat_id'])
-	
-	else:
-		session['chat_id']=content['message']['chat']['id']
-		fname=content['message']['chat']['first_name']
-		lname=content['message']['chat']['last_name']
-		name=fname+" "+lname
-		msg = content['message']['text']
-		start(name,msg)
+	elif 'text' in content['message']:
+		chat_id=str(content['message']['chat']['id'])
+	print session
+	for session_list in session:
+		if chat_id in session_list:
+			print "ho"
+			if session[chat_id][1]%5==0:
+				reply_markup = ReplyKeyboardMarkup([[telegram.KeyboardButton('Do you want to share Location', request_location=True)]],one_time_keyboard=False,resize_keyboard=True)
+				bot.sendMessage(session[chat_id][0], 'Sorry for interrupting!'+name, reply_markup=reply_markup)		
 		
+			if "callback_query" in content:
+				#t=time.time()
+				session[chat_id][5]=session[chat_id][5]-t
+				gans=content['callback_query']['data']
+				tcheck=content['callback_query']['message']['date']
+				internet_check=t-tcheck
+				if (t-int(internet_check))>10000 and session[chat_id][0]%4==0:
+					bot.sendMessage(session[chat_id][0], "It seems your internet not working properly. Don't worry your current status is saved:)" )
+				checkans(str(gans))
+				#print data
+				#i=i+1
+				takeques()
+
+			elif 'text' in content['message']:
+				fname=content['message']['chat']['first_name']
+				lname=content['message']['chat']['last_name']
+				name=fname+" "+lname
+				msg = content['message']['text']
+				start(name,msg)
+		
+			elif 'location' in content['message']:
+				longi = content['message']['location']['longitude']
+				lati = content['message']['location']['latitude']
+				fname=content['message']['chat']['first_name']
+				lname=content['message']['chat']['last_name']
+				name=fname+" "+lname
+				geolocator = GoogleV3()
+				#location = geolocator.reverse("52.509669, 13.376294")
+				location = geolocator.reverse((lati,longi))
+				adduser(name,str(location[0]))
+				bot.sendMessage(session[chat_id][0], text="The quiz is about to start.")
+				giveques()
+	
+		else:
+			print "not working"
+
 	return jsonify(content)
 	
 def start(name,msg):
+	global chat_id
 	if msg=='/start':
-		session['i']=1
-		session['stime']=strftime("%Y-%m-%d %H:%M:%S")
-		session['list']=[]
-		session['score']=0
+		session[chat_id]=[]
+		session[chat_id].append(chat_id)	#0.chat_id
+		session[chat_id].append(1)	#1.i
+		session[chat_id].append(strftime("%Y-%m-%d %H:%M:%S")) #2.stime
+		session[chat_id].append([]) #3.motion
+		session[chat_id].append(0)	#4.score
 		adduser(name,0)
 		reply_markup = ReplyKeyboardMarkup([[telegram.KeyboardButton('Do you want to share Location', request_location=True)]],one_time_keyboard=True,resize_keyboard=True)
-		bot.sendMessage(session['chat_id'], 'Hey '+name, reply_markup=reply_markup)
+		bot.sendMessage(session[chat_id][0], 'Hey '+name, reply_markup=reply_markup)
 	
-	if msg=='/stat':
+	elif msg=='/stat':
 		db=mysql.connect()
 		cursor=db.cursor()
-		cursor.execute("select avg(qtime) from userques where qid=%s",session['chat_id'])
+		cursor.execute("select avg(qtime) from userques where qid=%s",session[chat_id][1])
 		avg=cursor.fetchall()
-		cursor.execute("select score from user where uid=%s",session['chat_id'])
+		cursor.execute("select score from user where uid=%s",session[chat_id][0])
 		correct=cursor.fetchall()
-		bot.sendMessage(session['chat_id'], 'Hey'+name+' .Your score: '+correct'. You took on an average '+avg+' seconds to do a ques.')
-		cursor.execute("select location from user where uid=%s",session['chat_id'])
+		bot.sendMessage(session[chat_id][0], 'Hey'+name+' .Your score: '+correct+'. You took on an average '+avg+' seconds to do a ques.')
+		cursor.execute("select location from user where uid=%s",session[chat_id][0])
 		locat=cursor.fetchall()
 		cursor.execute("select name,score from user where location=%s",locat)
 		locat=cursor.fetchall()
-		bot.sendMessage(session['chat_id'], 'Scores of people near you: '+locat)
-		
-
+		for i in locat:
+			s=locat
+		bot.sendMessage(session[chat_id][0], 'Scores(out of 10) of people near you: '+locat)
+	
+	else:
+		checkans(msg)
 
 def adduser(uname,location):
+	global chat_id
 	db=mysql.connect()
 	cursor=db.cursor()
 	try:
-		cursor.execute("""INSERT INTO user (uid,name,stime,etime,location) VALUES (%s,%s,%s,%s,%s)""",(session['chat_id'],uname,session['stime'],0,location))
+		cursor.execute("""INSERT INTO user (uid,name,stime,etime,location) VALUES (%s,%s,%s,%s,%s)""",(session[chat_id][0],uname,session[chat_id][2],0,location))
 		#print "hi\n"
 		db.commit()
 		
 	except Exception as e:
-		cursor.execute("select location from user where uid=%s",session['chat_id'])
+		cursor.execute("select location from user where uid=%s",session[chat_id][0])
 		loc=cursor.fetchall()
 		if loc==location:
-			session['list'].append('stationary')
+			session[chat_id][3].append('stationary')	#list of motions
 		else:
-			session['list'].append("moving")
-		cursor.execute("""update user set location=%s,stime=%s where uid=%s""",(location,session['stime'],session['chat_id']))
+			session[chat_id][3].append("moving")
+		cursor.execute("""update user set location=%s,stime=%s where uid=%s""",(location,session[chat_id][2],session[chat_id][0]))
 		db.commit()
 
 def giveques():
+	global chat_id
 	db=mysql.connect()
 	cursor=db.cursor()
-	cursor.execute("select question from quesbank where qid=%s",session['i'])
+	cursor.execute("select question from quesbank where qid=%s",session[chat_id][1])
 	ques=cursor.fetchall()
-	#print ques
-	cursor.execute("select opta,optb,optc,optd from quesbank where qid=%s",session['i'])
+	cursor.execute("select answer from quesbank where qid=%s",session[chat_id][1])
+	cans=cursor.fetchall()
+	print cans
+	cursor.execute("select opta,optb,optc,optd from quesbank where qid=%s",session[chat_id][1])
 	option=cursor.fetchall()
-	if cans!='A' and cans!='C' and cans!='B' and cans!='D' :					#checking numeric or multiple
+	print option
+	if option[0][0]=='null' :				#checking numeric or multiple
 		keyboard = [
     ['7', '8', '9'],
     ['4', '5', '6'],
@@ -127,35 +146,38 @@ def giveques():
     ['-', '.', '0']
 ];
 		reply_markup = ReplyKeyboardMarkup(keyboard,resize_keyboard = True,one_time_keyboard = True)
-	#ans=str(ans)+str(chat_id)+str(i)
+		
 	else:
 		keyboard = [[InlineKeyboardButton(option[0][0], callback_data='A')],[InlineKeyboardButton(option[0][1], callback_data='B')],[InlineKeyboardButton(option[0][2], callback_data='C')],[InlineKeyboardButton(option[0][3], callback_data='D')] ]
 		reply_markup = InlineKeyboardMarkup(keyboard)
-	bot.sendMessage(session['chat_id'], text="Ques."+session['i']+" "+ques[0][0], reply_markup=reply_markup)
-	session['qtime']=time.time()
+	bot.sendMessage(session[chat_id][0], text="Ques."+str(session[chat_id][1])+" "+ques[0][0], reply_markup=reply_markup)
+	session[chat_id].append(time.time())	#5 is the qtime
 	
 	
 def checkans(gans):
+	global chat_id
 	db=mysql.connect()
 	cursor=db.cursor()
-	cursor.execute("select answer from quesbank where qid=%s",session['i'])
+	cursor.execute("select answer from quesbank where qid=%s",session[chat_id][1])
 	cans=cursor.fetchall()
+	print gans
+	print cans
 	if gans==cans:
 		status='correct'
-		session['score']+=1
+		session[chat_id][4]+=1
 	else:
 		status='incorrect'	
 	db=mysql.connect()
 	cursor=db.cursor()
 	try:
-		cursor.execute("INSERT INTO userques (uid,qid,canswer,ganswer,status,qtime,score) VALUES (%s,%s,%s,%s,%s,%s)",(session['chat_id'],session['i'],cans,gans,status,session['qtime'],session['score']))
+		cursor.execute("INSERT INTO userques (uid,qid,canswer,ganswer,status,qtime,score) VALUES (%s,%s,%s,%s,%s,%s)",(session[chat_id][0],session[chat_id][1],cans,gans,status,session[chat_id][5],session[chat_id][4]))
 		db.commit()
 	except Exception as e:
 		pass
-	cursor.execute("select exp from quesbank where qid=%s",session['i'])
+	cursor.execute("select exp from quesbank where qid=%s",session[chat_id][1])
 	exp=cursor.fetchall()
-	bot.sendMessage(session['chat_id'], text=str(status+". Explaination- "+str(exp)))
-	session['i']+=1
+	bot.sendMessage(session[chat_id][0], text=str(status+". Explaination- "+str(exp)))
+	session[chat_id][1]+=1
 
 
 if __name__ == "__main__":
